bb44ae4f7c79614517d6299c808c1ce17c7170ee
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-07-07 23:10:29 +0800

    初始化

---
+++ application/lib/enum/ErrorCode.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mingliang
+ * Date: 2018/10/19
+ * Time: 9:59 AM
+ */
+
+namespace app\lib\enum;
+
+
+class ErrorCode
+{
+    public static $OK = 0;
+    public static $IllegalAesKey = -41001;
+    public static $IllegalIv = -41002;
+    public static $IllegalBuffer = -41003;
+    public static $DecodeBase64Error = -41004;
+
+}
---
+++ config/alisms.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mingliang
+ * Date: 2018/11/29
+ * Time: 12:00 AM
+ */
+
+
+return [
+    "repair" => [
+        'AccessKeyId' => 'LTAIGjx5Z4Qj62ng', // 访问密钥，在阿里云的密钥管理页面创建
+        'AccessSecret' => '2Vww2c0X4yzEMnbnaT9Ll2YmmX1v3J', // 访问密钥，在阿里云的密钥管理页面创建
+        'TemplateDemandCode' => 'SMS_151991521', // 商家接单发送通知给用户
+        'TemplateServiceCode' => 'SMS_152280947', // 用户预约商家服务发送通知给服务
+        'SignName' => '智慧城市',
+    ],
+];
