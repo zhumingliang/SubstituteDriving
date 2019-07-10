bf2931f38c0ae7112b634233eca3d525e2e745d0
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-07-10 21:54:49 +0800

    初始化

--- application/api/service/OrderService.php
+++ application/api/service/OrderService.php
@@ -125,7 +125,7 @@ class OrderService
     public function handelDriverNoAnswer()
     {
         OrderPushT::where('state', OrderEnum::ORDER_PUSH_NO)
-            ->where('create_time', '<', date("Y-m-d H:i:s", time() - 30))
+            ->where('create_time', '<', date("Y-m-d H:i:s", time() - config('setting.driver_push_expire_in')))
             ->update(['state' => 4]);
     }
 
