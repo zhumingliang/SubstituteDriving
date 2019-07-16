f49b60751f5a8f7b2575a5842a13be1463b0ef99
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-07-17 02:02:40 +0800

    初始化

--- application/api/service/DriverService.php
+++ application/api/service/DriverService.php
@@ -214,7 +214,6 @@ class DriverService
         $drivers_info = DriverT::field('id,username')->whereIn('id', $ids)->select();
         foreach ($online as $k => $v) {
             foreach ($drivers_info as $k2 => $v2) {
-                echo $v['id'];
                 if ($v['id'] == $v2->id) {
                     $online[$k]['username'] = $v2->username;
                     unset($drivers_info[$k2]);
