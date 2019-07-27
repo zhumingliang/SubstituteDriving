ed82daacf7d94fb298b52a3de7a3a1bcf8f11927
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-07-27 18:17:41 +0800

    初始化

--- application/api/service/GatewayService.php
+++ application/api/service/GatewayService.php
@@ -8,19 +8,32 @@ use GatewayClient\Gateway;
 
 class GatewayService
 {
+
     public static function sendToDriverClient($u_id, $message)
     {
-        Gateway::sendToUid('driver' . '-' . $u_id, $message);
+        Gateway::sendToUid('driver' . '-' . $u_id, self::prefixMessage($message));
     }
 
     public static function sendToMiniClient($u_id, $message)
     {
-        Gateway::sendToUid('mini' . '-' . $u_id, $message);
+        Gateway::sendToUid('mini' . '-' . $u_id, self::prefixMessage($message));
     }
 
     public static function isDriverUidOnline($u_id)
     {
         return Gateway::isUidOnline('driver' . '-' . $u_id);
+
+    }
+
+    private static function prefixMessage($message)
+    {
+        $data = [
+            'errorCode' => 0,
+            'msg' => 'success',
+            'data' => $message
+
+        ];
+        return json_encode($data);
 
     }
 
