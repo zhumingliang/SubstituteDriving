95ed44b0e65f7acf2cd491125ca0045bd9d5c215
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-08-11 23:13:08 +0800

    初始化

--- application/api/service/WeixinService.php
+++ application/api/service/WeixinService.php
@@ -12,19 +12,6 @@ class WeixinService
 
     public function __construct()
     {
-      /*  $config = [
-            'app_id' => 'wxda0c645fd4945a61',
-            'secret' => 'eff35b3ff1f083e1e7e51045cfbf4a40',
-            'token' => 'tonglingok',
-            'response_type' => 'array',
-            //23xAerLQPXzNWHFRZQROvv4u7AHAmzPMa5ZzlRo3FnU
-        ];
-
-        $this->app = Factory::officialAccount($config);*/
-    }
-
-    public function validate()
-    {
         $config = [
             'app_id' => 'wxda0c645fd4945a61',
             'secret' => 'eff35b3ff1f083e1e7e51045cfbf4a40',
@@ -34,6 +21,10 @@ class WeixinService
         ];
 
         $this->app = Factory::officialAccount($config);
+    }
+
+    public function validate()
+    {
         $response = $this->app->server->serve();
         $response->send();
         exit;
