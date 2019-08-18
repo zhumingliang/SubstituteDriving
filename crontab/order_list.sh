2f05dc0511bec283b4ef3a3427493e46a49805ad
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-08-19 02:50:34 +0800

    初始化

--- crontab/order_list.sh
+++ crontab/order_list.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-
-if [ -f ~/.bash_profile ];
-then
-  . ~/.bash_profile
-fi
-
 step=1 #间隔的秒数，不能大于60
 for (( i = 0; i < 60; i=(i+step) ));
 do
