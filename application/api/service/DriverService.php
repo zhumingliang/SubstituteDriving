6cd2913fc986498ffa2318dfed6bcccb217534a6
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-08-20 09:18:06 +0800

    初始化

--- application/api/service/DriverService.php
+++ application/api/service/DriverService.php
@@ -27,8 +27,8 @@ class DriverService
 
     public function __construct()
     {
-      // $this->redis = new \Redis();
-      //  $this->redis->connect('127.0.0.1', 6379, 60);
+        $this->redis = new \Redis();
+        $this->redis->connect('127.0.0.1', 6379, 60);
 
     }
 
