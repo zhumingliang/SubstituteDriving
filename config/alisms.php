a191e8ea844684a743b6c17b00ba80b77acce414
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-08-26 22:46:07 +0800

    初始化

--- config/alisms.php
+++ config/alisms.php
@@ -20,8 +20,8 @@ return [
         'AccessSecret' => 'IGyopSctAoahUva0wP4wYDOMBuz4aq', // 访问密钥，在阿里云的密钥管理页面创建
         'TemplateRegisterCode' => 'SMS_166310718', // 小程序注册
         'TemplateLoginCode' => 'SMS_166310720', // 司机端登录
-        'TemplateDriverCode' => 'SMS_170116164', // 发送接单通知给司机端
+        'TemplateDriverCode' => 'SMS_172884242', // 发送接单通知给司机端
         'TemplateRechargeCode' => 'SMS_171357306', // 充值通知给司机
-        'SignName' => '奥凯代驾'
+        'SignName' => 'OK代驾'
     ],
 ];
