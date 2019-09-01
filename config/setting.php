a1b9003de4373b585cd5a27b23787688e1bd37a6
mingliangzhu <353575156@qq.com>
2019-09-01 16:23:52 +0800

    ceshi

--- config/setting.php
+++ config/setting.php
@@ -5,7 +5,7 @@ return [
     'driver_push_expire_in' => 20,
     'driver_nearby_km' => 10,
     'manager_nearby_km' => 300000,
-    'mini_nearby_km' => 2,
+    'mini_nearby_km' => 10,
     'balance_limit' => 10,
     'ticket_time' => 15,
     'token_cms_expire_in' => 7200,
