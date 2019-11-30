49ca63147d8308edf115f476c1fb6586c7e55582
zhumingliang <353575156@qq.com>
2019-12-01 00:39:45 +0800

    更新apk

--- application/api/model/TimeIntervalT.php
+++ application/api/model/TimeIntervalT.php
@@ -10,16 +10,6 @@ use think\Model;
 class TimeIntervalT extends Model
 {
 
-    public function getTimeBeginAttr($value)
-    {
-        return date('H:i', strtotime($value));
-
-    }
-
-    public function getTimeEndAttr($value)
-    {
-        return date('H:i', strtotime($value));
-    }
 
 
 }
--- application/api/service/OrderService.php
+++ application/api/service/OrderService.php
@@ -290,13 +290,6 @@ class OrderService
         }
         $day = date('Y-m-d', $dateTime);
         foreach ($interval as $k => $v) {
-            /* if (strtotime($v['time_begin']) < strtotime($v['time_end'])) {
-                 $time_begin = strtotime($v['time_begin']);
-                 $time_end = strtotime($v['time_end']);
-             } else {
-                 $time_begin = strtotime("-1 day", strtotime($v['time_begin']));
-                 $time_end = strtotime($v['time_end']);
-             }*/
             $time_begin = strtotime($day . ' ' . $v['time_begin']);
             $time_end = strtotime($day . ' ' . $v['time_end']);
             if ($time_begin <= $dateTime && $dateTime <= $time_end) {
