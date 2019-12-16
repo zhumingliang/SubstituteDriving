58b9cd1bebb3a38034d4960e16a5a0bbb81ae696
zhumingliang <353575156@qq.com>
2019-12-16 21:05:15 +0800

    修改推送时间

--- config/setting.php
+++ config/setting.php
@@ -2,7 +2,7 @@
 
 return [
     'domain' => 'https://tonglingok.com',
-    'driver_push_expire_in' => 30,
+    'driver_push_expire_in' => 45,
     'driver_nearby_km' => 20,
     'manager_nearby_km' => 300000,
     'mini_nearby_km' => 10,
