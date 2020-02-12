32b300d2b343172602f067d221d16f7b1410017a
zhumingliang <353575156@qq.com>
2020-02-13 00:21:16 +0800

    修改短信模块

--- config/queue.php
+++ config/queue.php
@@ -10,5 +10,30 @@
 // +----------------------------------------------------------------------
 
 return [
-    'connector' => 'Sync'
+    'connector'  => 'Redis',		    // 可选驱动类型：sync(默认)、Redis、database、topthink等其他自定义类型
+    'expire'     => 60,				    // 任务的过期时间，默认为60秒; 若要禁用，则设置为 null
+    'default'    => 'default',		    // 默认的队列名称
+    'host'       => '127.0.0.1',	    // redis 主机ip
+    'port'       => 6379,			    // redis 端口
+    'password'   => '',                 // redis 连接密码
+    'select'     => 1,				    // 使用哪一个 db，默认为 db0
+    'timeout'    => 0,				    // redis连接的超时时间
+    'persistent' => false,			    // 是否是长连接
 ];
+//    'connector' => 'Database',   // 数据库驱动
+//    'expire'    => 60,           // 任务的过期时间，默认为60秒; 若要禁用，则设置为 null
+//    'default'   => 'default',    // 默认的队列名称
+//    'table'     => 'jobs',       // 存储消息的表名，不带前缀
+//    'dsn'       => [],
+
+//    'connector'   => 'Topthink',	// ThinkPHP内部的队列通知服务平台 ，本文不作介绍
+//    'token'       => '',
+//    'project_id'  => '',
+//    'protocol'    => 'https',
+//    'host'        => 'qns.topthink.com',
+//    'port'        => 443,
+//    'api_version' => 1,
+//    'max_retries' => 3,
+//    'default'     => 'default',
+
+//    'connector'   => 'Sync',
